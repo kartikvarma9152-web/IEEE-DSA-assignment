e797c6ef06a0a9eb8a3dffef4a1b2b2f8ab6edd2
Kartikeya varma <kartikvarma9152@gmail.com>
2026-09-13 20:44:29 +0530

    Implement string permutation inclusion check

---
+++ stringpermutation.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean checkinclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) return false;
+        int[] count = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            count[s1.charAt(i) - 'a']++;
+            count[s2.charAt(i) - 'a']--;
+        }
+        if (iszero(count)) return true;
+        for (int i = s1.length(); i < s2.length(); i++) {
+            count[s2.charAt(i) - 'a']--;
+            count[s2.charAt(i - s1.length()) - 'a']++;
+            if (isZero(count)) return true;
+        }
+        return false;
+    }
+
+    private boolean iszero(int[] count) {
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) return false;
+        }
+        return true;
+    }
+}
